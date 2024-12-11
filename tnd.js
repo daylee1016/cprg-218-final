2b6738ce75b15231724b5a477ba0055c0268aa1e
daylee1016 <dayeong.lee@edu.sait.ca>
2024-12-10 20:19:44 -0700

    interactive buttons and pages
    added contents for each pages and made button interactive. Also changed some CSS

--- tnd.js
+++ tnd.js
@@ -1,5 +1,16 @@
+Home.addEventListener('click',function(){
+    console.log('Home Clicked');
+    
+})
+
 Portfolio.addEventListener('click',function(){
     console.log('Portfolio Clicked');
-    h2.innerHTML = "T&D Portfolio";
+
+    
+})
+
+ContactUs.addEventListener('click',function(){
+    console.log('Contact Us Clicked');
+
     
 })
